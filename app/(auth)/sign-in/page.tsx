b5b73b54c718f1cb33f307877d83a2b29718c1fa
Bilal <bilal.kanjelkheir@gmail.com>
2025-04-21 23:33:33 +0300

    setup routing, auth pages, and app overall

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import AuthForm from "@/components/authForm";
+
+const SignIn = () => {
+    return <AuthForm type="sign-in" />
+}
+export default SignIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import AuthForm from "@/components/authForm";
+
+const SignUp = () => {
+    return <AuthForm type="sign-up" />
+}
+export default SignUp
